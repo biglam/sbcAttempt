fb96cf433f08f2a69a66bcc23bf8e22dd9ac4e2e
biglam <craig@techiteasy.org.uk>
2016-07-12 21:38:45 +0100

    added configurer suffix

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.test.bookpub.formatters.BookFormatter;
 import org.test.bookpub.repository.BookRepository;
 
-/**
- * Created by lam on 12/07/16.
- */
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Bean
@@ -38,6 +36,10 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         registry.addFormatter(new BookFormatter(bookRepository));
     }
 
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(true);
+    }
 //    @Bean
 //    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
 //        return new ByteArrayHttpMessageConverter();
