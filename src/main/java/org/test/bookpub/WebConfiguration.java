8ad4281104c7d72d81ad8dcdb1f1c5b27a098062
biglam <craig@techiteasy.org.uk>
2016-07-12 20:13:35 +0100

    added locale strategy

--- src/main/java/org/test/bookpub/WebConfiguration.java
+++ src/main/java/org/test/bookpub/WebConfiguration.java
@@ -3,14 +3,27 @@ package org.test.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * Created by lam on 12/07/16.
  */
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
